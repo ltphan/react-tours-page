1bed68e49b1fda1fca1262d76ad108966f0075b0
Linh Phan <phanthuclinh@gmail.com>
2021-08-10 14:46:40 -0700

    rendering list item on home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import { useState } from "react"
+
 function App() {
+  const [blogs, setBlogs] = useState([
+    {id: 1, name: "Blog1", body: "Blah blah blah"},
+    {id: 2, name: "Blog2", body: "Blah blah blah"},
+    {id: 3, name: "Blog3", body: "Blah blah blah"}
+  ])
+  console.log("blogs", blogs)
+
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id)
+    setBlogs(newBlogs)
+    console.log("blogs filtered", blogs)
+  }
+
   return (
-    <h2>Tours Page</h2>
+    <div>
+      {blogs.map(blog => {
+        return (
+      <div key={blog.id}>
+        <li>{blog.name}</li>
+        <button onClick={() => handleDelete(blog.id)}>{"Delete"}</button>
+      </div>
+        )
+      })}
+    </div>
   );
 }
 
