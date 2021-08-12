08decc0a13e7eedbd35febce0d8525ed54e9f500
Linh Phan <phanthuclinh@gmail.com>
2021-08-12 08:11:47 -0700

    move mock data to data/index file

---
+++ src/data/index.js
@@ -0,0 +1,7 @@
+export default [
+    {id: 1, name: "Blog1", body: "Blah blah blah"},
+    {id: 2, name: "Blog2", body: "Blah blah blah"},
+    {id: 3, name: "Blog3", body: "Blah blah blah"},
+    {id: 4, name: "Blog4", body: "Blah blah blah"},
+    {id: 5, name: "Blog5", body: "Blah blah blah"}
+]
