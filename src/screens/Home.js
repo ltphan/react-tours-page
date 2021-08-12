0ee8ce18ed1f1ae989e0abbbd71c0774b3786b4f
Linh Phan <phanthuclinh@gmail.com>
2021-08-12 08:22:18 -0700

    create home component

---
+++ src/screens/Home.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+import mockData from "../data/index"
+
+const Home = () => {
+    const [blogs, setBlogs] = useState(mockData)
+
+    const handleDelete = (id) => {
+      const newBlogs = blogs.filter(blog => blog.id !== id)
+      setBlogs(newBlogs)
+    }
+  
+    return (
+      <div>
+        {blogs.map(blog => {
+          return (
+        <div key={blog.id}>
+          <li>{blog.name}</li>
+          <button onClick={() => handleDelete(blog.id)}>{"Delete"}</button>
+        </div>
+          )
+        })}
+      </div>
+    );
+}
+
+export default Home
