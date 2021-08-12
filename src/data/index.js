605b4f4d1c1a00d600646a137d1aa94ce0ee3d9c
Linh Phan <phanthuclinh@gmail.com>
2021-08-12 08:34:18 -0700

    assign value to a variable

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,9 @@
-export default [
+const mockData = [
     {id: 1, name: "Blog1", body: "Blah blah blah"},
     {id: 2, name: "Blog2", body: "Blah blah blah"},
     {id: 3, name: "Blog3", body: "Blah blah blah"},
     {id: 4, name: "Blog4", body: "Blah blah blah"},
     {id: 5, name: "Blog5", body: "Blah blah blah"}
 ]
+
+export default mockData
