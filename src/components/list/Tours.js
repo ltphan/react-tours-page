2e0c78486f35eea8da2c801a8a99410ea97261b4
Linh Phan <phanthuclinh@gmail.com>
2021-08-12 09:03:24 -0700

    add conditional rendering in JSX tempate

--- src/components/list/Tours.js
+++ src/components/list/Tours.js
@@ -1,18 +1,24 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import mockData from "../../data/index"
 
 const Tours = () => {
-    const [blogs, setBlogs] = useState(mockData)
+    const [blogs, setBlogs] = useState(null)
+    console.log(blogs)
 
     const handleDelete = (id) => {
       const newBlogs = blogs.filter(blog => blog.id !== id)
       setBlogs(newBlogs)
     }
+
+    useEffect(() => {
+        setBlogs(mockData)
+        console.log("useEffect runs after every render")
+    },[])
   
     return (
       <div>
-        {blogs.map(blog => {
+        {blogs && blogs.map(blog => {
           return (
         <div key={blog.id}>
           <li>{blog.name}</li>
