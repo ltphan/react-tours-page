5407e594b3d1aeb9f19f3b26d121c0656f1e824a
Linh Phan <phanthuclinh@gmail.com>
2021-08-13 11:02:06 -0700

    show loading longer before data shows

--- src/components/list/Tours.js
+++ src/components/list/Tours.js
@@ -2,13 +2,17 @@ import { useEffect, useState } from "react"
 
 import { fetchTours } from "../../data/index"
 import Tour from "../../components/item/Tour"
+import Loading from "../../components/item/Loading"
+
 
 const Tours = () => {
     const [tours, setTours] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     const fetchData = async () => {
         const result = await fetchTours()
         setTours(result)
+        setLoading(false)
     }
 
     const handleDelete = (id) => {
@@ -17,11 +21,12 @@ const Tours = () => {
     }
 
     useEffect(() => {
-        fetchData()
+        setTimeout(fetchData, 1000)
     },[])
   
     return (
       <div>
+        {loading && <Loading />}
         {tours && tours.map(tour => {
           return <Tour key={tour.id} name={tour.name} handleDelete={() => handleDelete(tour.id)}/>
         })}
