ca570b07af9eff67a0590de3e110eebfa10d55b9
Linh Phan <phanthuclinh@gmail.com>
2021-08-13 11:51:41 -0700

    add error state handling

--- src/components/list/Tours.js
+++ src/components/list/Tours.js
@@ -3,16 +3,22 @@ import { useEffect, useState } from "react"
 import { fetchTours } from "../../data/index"
 import Tour from "../../components/item/Tour"
 import Loading from "../../components/item/Loading"
-
+import Error from "../../components/item/Error"
 
 const Tours = () => {
     const [tours, setTours] = useState(null)
     const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
 
     const fetchData = async () => {
+      try {
         const result = await fetchTours()
         setTours(result)
+      } catch (e) {
+        setError(true)
+      } finally {
         setLoading(false)
+      }
     }
 
     const handleDelete = (id) => {
@@ -30,6 +36,7 @@ const Tours = () => {
         {tours && tours.map(tour => {
           return <Tour key={tour.id} name={tour.name} handleDelete={() => handleDelete(tour.id)}/>
         })}
+        {error && <Error />}
       </div>
     );
 }
