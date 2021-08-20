53d1cb1610bc48dddb5639a57189f45f86a686b4
Linh Phan <phanthuclinh@gmail.com>
2021-08-20 11:04:32 -0700

    add CORS policy to allow browser to access server data

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,6 @@
 export async function fetchTours() {
     try {
-        const response = await fetch("https://course-api.com/react-tours-project")
+        const response = await fetch("https://course-api.com/react-tours-project", {"Access-Control-Allow-Origin": "*"})
         const data = await response.json()
         return data
     } catch (e) {
