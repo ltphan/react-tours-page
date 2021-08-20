235e52e2f7b3252aefb6bd194828939e83a69fbe
Linh Phan <phanthuclinh@gmail.com>
2021-08-20 11:05:22 -0700

    add react router routing at top level of react app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Home from "./screens/Home"
+import About from "./screens/About"
+import NavBar from "./navigation/NavBar"
 
 function App() {
-  return <Home />
+  return (
+  <Router>
+    <NavBar/>
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+    </Switch>
+  </Router>
+  )
 }
 
 export default App;
