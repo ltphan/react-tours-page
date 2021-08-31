6d82e529ad3f19e161dc41688f03983e407a1474
Linh Phan <phanthuclinh@gmail.com>
2021-08-31 13:50:03 -0700

    no re-renders for useRef

--- src/screens/Form.js
+++ src/screens/Form.js
@@ -1,7 +1,21 @@
+import { useRef, useState } from "react"
+
 const Form = () => {
+    const [count, setCount] = useState(0)
+    const countRef = useRef()
+    countRef.current = count
+
+    const handleClick = () => {
+        countRef.current++ 
+        setCount(countRef.current)
+    }
+
+    console.log("I rendered")
+
     return (
         <div>
-            {"FORMS"}
+            <button onClick={handleClick}>{"Click Me"}</button>
+            <div>{countRef.current}</div>
         </div>
     )
 }
