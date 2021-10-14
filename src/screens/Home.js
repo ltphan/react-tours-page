fa6ef4988ea26852d4da6938d9fa0e9d6f7ad201
Linh Phan <phanthuclinh@gmail.com>
2021-10-13 18:59:51 -0700

    basics of useRef

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,30 @@
+import { useRef } from "react"
+
 import Tours from "../components/list/Tours"
 
 const Home = () => {
-  return <Tours />
+
+  const inputEl = useRef(null)
+  let numRef = useRef(0)
+
+  const onButtonClicked = () => {
+    console.log(inputEl.current)
+    inputEl.current.focus()
+  }
+
+  const increment = () => {
+    console.log(numRef.current)
+    numRef.current+=1
+  }
+  console.log("render")
+
+  return (
+  <>
+    <input ref={inputEl} type="text"/>
+    <button onClick={onButtonClicked}>Focus the input</button>
+    <button onClick={increment}>Increment</button>
+  <Tours />
+  </>)
 }
 
 export default Home
